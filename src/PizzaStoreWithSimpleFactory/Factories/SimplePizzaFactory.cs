dc916f2048eea9a1a617ae4b45715459bfab226b
Matheus Oliveira <matheus.oliveira@msn.com>
2025-01-21 20:31:06 -0300

    add simple factory

--- src/PizzaStoreWithSimpleFactory/Factories/SimplePizzaFactory.cs
+++ src/PizzaStoreWithSimpleFactory/Factories/SimplePizzaFactory.cs
@@ -1,9 +1,31 @@
-namespace PizzaStoreWithSimpleFactory;
+using PizzaStoreWithSimpleFactory.Pizzas;
+using PizzaStoreWithSimpleFactory.Pizzas.Abstractions;
+
+namespace PizzaStoreWithSimpleFactory.Factories;
 
 public class SimplePizzaFactory
 {
     public Pizza CreatePizza(string type)
     {
         Pizza pizza = null;
+        
+        if (type.Equals("cheese"))
+        {
+            pizza = new CheesePizza();
+        }
+        else if (type.Equals("pepperoni"))
+        {
+            pizza = new PepperoniPizza();
+        }
+        else if (type.Equals("clam"))
+        {
+            pizza = new ClamPizza();
+        }
+        else if (type.Equals("veggie"))
+        {
+            pizza = new VeggiePizza();
+        }
+
+        return pizza;
     }
 }
--- src/PizzaStoreWithSimpleFactory/Pizzas/PepperoniPizza.cs
+++ src/PizzaStoreWithSimpleFactory/Pizzas/PepperoniPizza.cs
@@ -1,6 +1,18 @@
+using PizzaStoreWithSimpleFactory.Pizzas.Abstractions;
+
 namespace PizzaStoreWithSimpleFactory.Pizzas;
 
-public class PepperoniPizza
+public class PepperoniPizza : Pizza
 {
+    public PepperoniPizza()
+    {
+        Name = "Pepperoni Pizza";
+        Dough = "Crust";
+        Sauce = "Marinara sauce";
+
+        Toppings.Add("Sliced Pepperoni");
+        Toppings.Add("Sliced Onion");
+        Toppings.Add("Grated parmesan cheese");
+    }
     
 }
